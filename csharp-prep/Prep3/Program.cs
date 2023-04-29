1d9e7cbfa3c58d6085d6cb9ebfb58b2f309a818d
amnanozi <musitwaa@gmail.com>
2023-04-29 06:54:36 +0300

    week 1 work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 102);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine(" Too higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Too lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+
+        }                    
+
     }
+
 }
